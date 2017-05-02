b9590881272317f31e2fe50bc8d67a9b94b8d05f
farizrahman4u <farizrahman4u@gmail.com>
2017-05-02 12:38:24 +0530

    add eval test

---
+++ tests/test_eval.py
@@ -0,0 +1,28 @@
+from ktorch import *
+import numpy as np
+
+
+x = np.random.random((2, 3, 4))
+y = np.random.random((3, 4))
+
+# Imperative
+
+a = Variable(x)
+b = Variable(y)
+
+c = a + b * 0.3
+
+c.eval()
+
+assert c[:1, :2].eval().shape == (1, 2, 4)
+
+# Symbolic
+
+a = Tensor()
+b = Tensor()
+
+d = a + b * 0.3
+
+f = Function([a, b], d)
+
+assert np.all(f([x, y]) == c.eval())
